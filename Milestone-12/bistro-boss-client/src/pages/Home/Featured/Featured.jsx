a98f445cc97f0365602c6579b2f0a0165c339a00
Hasib <hhasibul231@gmail.com>
2023-05-31 08:40:34 +0600

    work on featured item

--- Milestone-12/bistro-boss-client/src/pages/Home/Featured/Featured.jsx
+++ Milestone-12/bistro-boss-client/src/pages/Home/Featured/Featured.jsx
@@ -6,28 +6,31 @@ import './Featured.css'
 const Featured = () => {
     return (
       <div className="featured-item text-white pt-8 my-20 bg-fixed  ">
-        <SectionTitle
-          subHeading={"check it out"}
-          heading={"Featured Item"}
-        ></SectionTitle>
-        <div className="md:flex justify-center items-center pb-20 pt-12 px-36 px-16 bg-opacity-60 bg-black">
-          <div>
-            <img src={featuredImg} alt="" />
-          </div>
-          <div className="md:ml-10">
-            <p>Aug 20, 2029</p>
-            <p className="uppercase">Where can i get some?</p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-              expedita hic dolorem, iusto vel suscipit nam excepturi debitis
-              magnam nostrum! Ut eum dignissimos culpa doloremque eligendi
-              consectetur blanditiis laboriosam fugiat ea quia similique quam
-              nisi reprehenderit numquam magnam nemo vitae cupiditate, atque
-              maiores dicta minus pariatur. Perspiciatis nobis vero quas?
-            </p>
-            <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">
-              Order Now
-            </button>
+        <div className="bg-opacity-60 bg-black pt-1">
+          <SectionTitle
+            subHeading={"check it out"}
+            heading={"Featured Item"}
+          ></SectionTitle>
+          <div className="md:flex justify-center items-center pb-20 pt-12 px-36 ">
+            <div>
+              <img src={featuredImg} alt="" />
+            </div>
+            <div className="md:ml-10">
+              <p>Aug 20, 2029</p>
+              <p className="uppercase">Where can i get some?</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate expedita hic dolorem, iusto vel suscipit nam excepturi
+                debitis magnam nostrum! Ut eum dignissimos culpa doloremque
+                eligendi consectetur blanditiis laboriosam fugiat ea quia
+                similique quam nisi reprehenderit numquam magnam nemo vitae
+                cupiditate, atque maiores dicta minus pariatur. Perspiciatis
+                nobis vero quas?
+              </p>
+              <button className="btn btn-outline border-0 border-b-4 mt-4 text-white">
+                Order Now
+              </button>
+            </div>
           </div>
         </div>
       </div>
